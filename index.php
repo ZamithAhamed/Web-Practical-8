61375c395dae45097657f65b38ac96a2d7071c33
ZamithAhamed <zamith11@gmail.com>
2022-09-06 22:57:06 +0530

    login page created

--- index.php
+++ index.php
@@ -9,23 +9,23 @@
 </head>
 <body style="background-image: url(https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bG9naW58ZW58MHx8MHx8&w=1000&q=80); background-size: 100%  115% ; background-repeat: no-repeat; background-attachment: fixed; background-position: 0px -22px;">
 <div >
-	<br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 	<h1 align="center">Login Form</h1><br>
-	<form action="loogin.php">
+	<form action="login.php" method="POST">
 	<table align="center">
 		<tr>
 			<td><label>Username</label></td>
-			<td><input type="text" style=""></label></td>
+			<td><input type="text" name="uname" required></label></td>
 		</tr>
 		<tr>
 			<td><label>Password</label></td>
-			<td><input type="Password"></label></td>
+			<td><input type="Password" name="pass" required></label></td>
 		</tr>
 		<tr>
 			<td></td>
-			<td align="right"><input type="checkbox"><span style="font-weight: bold; color: floralwhite;">Remember me</span>
-							<button style="border-color: Green;">Login</button>
+			<td align="right"><input type="checkbox"><span style="font-weight: bold; color: black;">Remember me</span>
+							<button type="submit" style="border-color: Green;">Login</button>
 							<button type="reset" style="color: red; border-color: red;">Cancel</button>
 			</td>
 		</tr>
--- login.php
+++ login.php
@@ -12,4 +12,21 @@ if (!$con) {
     echo "There are some problems with connceting to the database";
 } 
 
+
+$uname = $_POST['uname'];
+$pass = $_POST['pass'];
+
+$sql = "SELECT * FROM $table WHERE Username = '$uname' AND Pass = '$pass' ";
+$result = mysqli_query($con,$sql) or die(mysql_error());
+$rows = mysqli_num_rows($result);
+
+if($rows > 0){
+    echo '<h2>Welcome</h2>';
+    echo '<script> alert("LOGGED IN) </script>';
+}
+else{
+    echo '<script> alert("Invald Credentials") </script>';
+    header("Location: home.html");
+}    
+
 ?>
